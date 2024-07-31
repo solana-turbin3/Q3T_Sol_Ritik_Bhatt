cd6f39981ff228f6f109dc633daedff766e502d2
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-07-31 15:39:10 +0530

    Feat: Initialized metadata successfully

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,15 +1,19 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
     CreateMetadataAccountV3InstructionAccounts, 
     CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+    DataV2Args,
+    Collection,
+    Creator,
+    Uses
 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("5Bueyt5zFxygKMuESUiKmhE3k3yhDFQeHvdSWWjNREUp")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -20,28 +24,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: mint,
+            mintAuthority: signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "Happy Token",
+            symbol: "HPT",
+            uri: "https://bafybeidpf3uzi2i7zn4pgczcimbgeubmrn3fw3kltl6ndhoxqddasvepce.ipfs.w3s.link/metadata.json",
+            sellerFeeBasisPoints: 0,
+            creators: null as Creator[] | null,
+            collection: null as Collection | null,
+            uses: null as Uses | null,
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            isMutable: true,
+            collectionDetails: null,
+            data: data
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
+        // 52oHb1bHTq7LxgWDHKd4aQVioF8CaWJ5gYMXsKAyD9BtVCb95ECxdaDhukpsWMqH6JcLa4z3BSYmnyVcBTadaza6
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
