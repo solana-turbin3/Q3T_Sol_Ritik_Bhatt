e5466ec7e403332210d213bcd2c92d73413369b5
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-03 01:28:39 +0530

    Feat: Successfully minted Ritik Rug nft

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,22 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    let tx = await createNft(umi, {
+        mint,
+        name: "Ritik Rug NFT",
+        symbol: "RRT",
+        uri: "https://arweave.net/YZZYkANQlv8bQcpueYOisU5-o5eCmejr6ZcFLlQDCEE",
+        sellerFeeBasisPoints: percentAmount(1)
+    });
 
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
     console.log("Mint Address: ", mint.publicKey);
 })();
+
+// success -> Succesfully Minted! Check out your TX here:
+// https://explorer.solana.com/tx/fSrQ3EgP7Xr8E5DWeg6ief7762Ho18PfpGuwjB2eSzVN5iHSM1ddCeeLWm8o1AGEowpvVWT7LRuEmiWD8kbUPNq?cluster=devnet
+// Mint Address:  2rggwNkawvNZrUckADYPBfvxUunEGCnck1UhEgfHhGR4
+// Done in 20.92s.
