694c0d034ee7b7159dbfe566f8daae1fcd2c3de6
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-02 23:05:30 +0530

    Feat: nft image added on arweave ipfs

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,8 +1,8 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "./wallet/wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -15,16 +15,22 @@ umi.use(signerIdentity(signer));
 
 (async () => {
     try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+        // 1. Load image
+        const imagePath = '/home/ritikbhatt020/wba-turbin3-cohort-q3/ts/assets/generug.png';  // specify your image path
+        const imageBuffer = await readFile(imagePath);
 
-        // const image = ???
+        // 2. Convert image to generic file
+        const generic = createGenericFile(imageBuffer, "rug", {
+            contentType: "image/png"
+        });
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
+        // 3. Upload image
+        const [myUri] = await umi.uploader.upload([generic]);
+        console.log("Your image URI: ", myUri);
+    } catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
+
+// success -> Your image URI:  https://arweave.net/mvrjlFa_rmOTCgI32xCWYK3VuZt9RbMFW9o4bYbW5D0
+// Done in 7.88s.
