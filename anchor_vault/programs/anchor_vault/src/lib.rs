6b6c8a7f2b7a057a2b30e5a1f8416d5f53ec41a6
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-15 23:16:54 +0530

    feat: implemented vault state account and initialize context

--- anchor_vault/programs/anchor_vault/src/lib.rs
+++ anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::{entrypoint::ProgramResult, entrypoint_deprecated::ProgramResult}};
 
 declare_id!("vF7jhRztdWk47aQkYwHJa3Gfo4FwLyUXngYhy2PdAEs");
 
@@ -13,4 +13,39 @@ pub mod anchor_vault {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump,
+        space = VaultState::INIT_SPACE,
+    )]
+    pub state: Account<'info, VaultState>,
+    #[account(
+        seeds = [b"vault", state.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info>  {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> ProgramResult {
+        self.state.vault_bump = bumps.vault;
+        self.state.state_bump = bumps.state;
+        Ok(())
+    }
+}
+
+#[account]
+pub struct VaultState {
+    pub vault_bump: u8,
+    pub state_bump: u8,
+}
+
+impl Space for VaultState {
+    const INIT_SPACE: usize = 8 + 1 + 1;
+}
