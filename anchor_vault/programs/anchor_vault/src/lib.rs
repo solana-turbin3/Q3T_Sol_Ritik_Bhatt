d5b148d5cf974d5fe46136ae911c3d0ce792f5ac
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-16 01:25:45 +0530

    feat: completed deposit ctx plus implementation fn

--- anchor_vault/programs/anchor_vault/src/lib.rs
+++ anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::{prelude::*, solana_program::{entrypoint::ProgramResult, entrypoint_deprecated::ProgramResult}};
+use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
 
 declare_id!("vF7jhRztdWk47aQkYwHJa3Gfo4FwLyUXngYhy2PdAEs");
 
@@ -33,9 +33,46 @@ pub struct Initialize<'info> {
 }
 
 impl<'info> Initialize<'info>  {
-    pub fn initialize(&mut self, bumps: &InitializeBumps) -> ProgramResult {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
         self.state.vault_bump = bumps.vault;
         self.state.state_bump = bumps.state;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", state.key().as_ref()],
+        bump = state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = state.state_bump,
+    )]
+    pub state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit(&mut self, amount: u64) -> Result<()> {
+
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.user.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, amount)?;
+
         Ok(())
     }
 }
