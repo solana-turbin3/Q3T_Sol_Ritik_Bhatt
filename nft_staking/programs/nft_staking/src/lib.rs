76a88a1cd466160d0e7c1be710756e720ab55263
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-03 05:13:48 +0530

    feat: initialized stake config

--- nft_staking/programs/nft_staking/src/lib.rs
+++ nft_staking/programs/nft_staking/src/lib.rs
@@ -1,16 +1,24 @@
 use anchor_lang::prelude::*;
 
+pub mod state;
+pub mod instructions;
+
+pub use instructions::*;
+
 declare_id!("VAzNjZyA4Td3mcnkvooVfwarDFkhKPdmFBxQaMb9KRW");
 
 #[program]
 pub mod nft_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(
+        ctx: Context<InitializeConfig>,
+        points_per_stake: u8,
+        max_stake: u8,
+        freeze_period: u32,
+    ) -> Result<()> {
+        ctx.accounts
+            .initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
